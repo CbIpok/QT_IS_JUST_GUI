50010dfec222b0ebe9b73d32b88b085c86f4db2d
FTBAngel <snapgod12@gmail.com>
2025-09-21 23:35:27 +1000

    delete useless trash

--- check_tests.py
+++
@@ -1,6 +0,0 @@
-﻿import pathlib
-exe = pathlib.Path(r"build/Release/cursedarch_tests.exe")
-print(exe.exists())
-if exe.exists():
-    data = exe.read_bytes()
-    print('HashTableTest' if b'HashTableTest' in data else 'no-hash')
